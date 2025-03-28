c6fd03ace09f2aa7a61acf298165cfa340c08839
Seth M <54655028+sethmorton@users.noreply.github.com>
2025-03-28 18:59:01 -0400

    feat: add email protection to employee dashboard page

--- src/app/(protected)/employee/page.tsx
+++ src/app/(protected)/employee/page.tsx
@@ -1,6 +1,20 @@
+// import { auth } from '@clerk/nextjs';
+import { auth, clerkClient } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+import { ALLOWED_EMAILS } from '@/src/constants';
 import DashboardClient from '@/src/features/employee-dashboard/components/DashboardClient';
 
-// This is a server component that wraps the client component
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const clerk = await clerkClient();
+  const { userId } = await auth();
+  const user = await clerk.users.getUser(userId ?? '');
+  const email = user.emailAddresses[0]?.emailAddress;
+
+  // Redirect to explore if email not in allowed list
+  if (!email || !ALLOWED_EMAILS.includes(email)) {
+    redirect('/explore');
+  }
+
   return <DashboardClient />;
 }
